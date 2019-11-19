0922545c0705dd74ca7e01ea0f58683ccab6be5d
Yao-Phoenix <57624183+Yao-Phoenix@users.noreply.github.com>
2019-11-19 16:49:35 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -7,17 +7,19 @@ try:
 except:
     print("Parameter Error")
     sys.exit()
-if salary - 5000 <= 3000:
+if salary - 5000 <= 0:
+    print("0")
+elif salary - 5000 <= 3000:
     print("{:.2f}".format((salary - 5000) * 0.03))
-elif 3000 < salary - 5000 <= 12000:
+elif salary - 5000 <= 12000:
     print("{:.2f}".format((salary - 5000) * 0.1 - 210))
-elif 12000 < salary - 5000 <= 25000:
+elif salary - 5000 <= 25000:
     print("{:.2f}".format((salary - 5000) * 0.2 - 1410))
-elif 25000 < salary - 5000 <= 35000:
+elif salary - 5000 <= 35000:
     print("{:.2f}".format((salary - 5000) * 0.25 - 2660))
-elif 35000 < salary - 5000 <= 55000:
+elif salary - 5000 <= 55000:
     print("{:.2f}".format((salary - 5000) * 0.3 - 4410))
-elif 55000 < salary - 5000 <= 80000:
+elif salary - 5000 <= 80000:
     print("{:.2f}".format((salary - 5000) * 0.35 - 7160))
 elif salary - 5000 > 80000:
     print("{:.2f}".format((salary - 5000) * 0.45 - 15160))
