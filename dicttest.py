3fbd65ff4e182d98b6ad40e1e52f769d0dded1cf
Yao-Phoenix <493867456@qq.com>
2019-11-20 14:56:24 +0800

    函数－数据整理

--- dicttest.py
+++ dicttest.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
-
 import sys
 
+output_dict = {}
+def handle_data(str_):
+    id,name = str_.split(':')
+    output_dict[id] = name
+
+def print_data(k,v):
+    print("ID: {} Name: {}".format(k,v))
+
+
 if __name__ == '__main__':
-    courses = dict()
-    for argv in sys.argv[1:]:
-        id,name = argv.split(':')
-        courses[id] = name
-    for key,value in courses.items():
-        print("ID: {} Name: {}".format(key,value))
+
+    for arg in sys.argv[1:]:
+        handle_data(arg)
+
+    for key in output_dict:
+        print_data(key, output_dict[key])
