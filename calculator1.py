0213c719be639fd9d48892682f09ad3dc91d4ef5
Yao-Phoenix <493867456@qq.com>
2019-11-20 17:34:00 +0800

    improve

---
+++ calculator1.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import sys
+"""
+def salary_insurance(salary):
+    insurance = salary * 0.165
+    return insurance
+
+def income(salary):
+    insurance = salary_insurance(salary)
+    income = salary - insurance -5000
+    return income
+"""
+def result(salary):
+    income_ = salary - salary * 0.165 - 5000
+    if income_ <= 0:
+        result = 0
+    elif income_ <= 3000:
+        result = income_ * 0.03
+    elif income_ <= 12000:
+        result = income_ * 0.1 - 210
+    elif income_ <= 25000:
+        result = income_ * 0.2 - 1410
+    elif income_ <= 35000:
+        result = income_ * 0.25 - 2660
+    elif income_ <= 55000:
+        result = income_ * 0.30 - 4410
+    elif income_ <= 80000:
+        result = income_ * 0.35 - 7160
+    else:
+        result = income * 0.45 - 15160
+    Salary = salary - salary * 0.165 - result
+    return Salary
+
+if __name__ == '__main__':
+    input_ = dict()
+    output_ = dict()
+    for argv in sys.argv[1:]:
+        id, salary = argv.split(':')
+        try:
+            input_[id] = int(salary)
+        except:
+            print("Parameter Error")
+            exit()
+    Salary = input_.values()
+    ID = input_.keys()
+   # print(Salary)
+    for i in Salary:
+        Result = result(i)
+        for j in ID:
+            output_.setdefault(j, Result)
+            continue
+    for key, value in output_.items():
+        print("{}:{:.2f}".format(key,value))
