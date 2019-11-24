58e843ff8b6d994d3cada20b1eb3d717346a8c32
Yao-Phoenix <493867456@qq.com>
2019-11-24 13:40:32 +0800

    sum

---
+++ sumtest.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    a = int(input("Please enter the first num: "))
+    b = int(input("Please enter the second num: "))
+    c = int(input("Please enter the third num: "))
+    print("a is {}, b is {}, c is {}, a+b+c = {}".format(a, b, c, a+b+c))
