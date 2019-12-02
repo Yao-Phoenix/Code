4a99a638b99fcf0e0ef36254603eeea3f62f10cc
Yao-Phoenix <493867456@qq.com>
2019-12-02 09:08:49 +0800

    flask2

---
+++ flask-test-app/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template,abort
+
+app = Flask(__name__)
+
+@app.route('/user/<username>')
+def user_index(username):
+    if username == 'invalid':
+        abort(404)
+    return render_template('user_index.html', username=username)
+
+if __name__ == '__main__':
+    app.run()
+
